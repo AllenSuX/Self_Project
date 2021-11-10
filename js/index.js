9c1ca61cbdcea9a9a549f12863b04a8b159c9c96
AllenSuX <j850711k@yahoo.com.tw>
2021-11-10 23:16:04 +0800

    hover測試2

--- js/index.js
+++ js/index.js
@@ -53,14 +53,16 @@ var to_top = document.getElementsByClassName("arrow_img")[0];
     to_top.addEventListener("click",function(){
     $("html, body").animate({scrollTop: 0}, 800);
 
-    var mobileHover = function () {
-        $('*').on('touchstart', function () {
-            $(this).trigger('hover');
-        }).on('touchend', function () {
-            $(this).trigger('hover');
-        });
-    };
 });
+
+var mobileHover = function () {
+    $('*').on('touchstart', function () {
+        $(this).trigger('hover');
+    }).on('touchend', function () {
+        $(this).trigger('hover');
+    });
+};
+mobileHover();
 
   
    
